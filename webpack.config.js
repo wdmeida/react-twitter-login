a30b47df1e33f8c335e6830e9689c232e5321160
Wagner Almeida <wdmeida@gmail.com>
2018-02-26 15:34:58 -0300

    Initial config

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+const include = join(__dirname, 'src');
+
+export default {
+  entry: './index',
+  output: {
+    path: join(__dirname, 'dist'),
+    libraryTarget: 'umd',
+    library: 'ReactTwitterLogin',
+  },
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.(js|jsx)$/,
+        loader: 'babel-loader',
+        include,
+      },
+    ],
+  },
+};
